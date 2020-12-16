5c48d468c3635898abae035cc954409f85a7b69f
ZantetsukenGT <superspeed52@gmail.com>
2020-12-15 23:36:40 -0600

    Agregar usuario

--- routes/usuario.route.js
+++ routes/usuario.route.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var con = require('../dbcontroller/dbconnection');
 
 router.get('/validate/admin', function(req, res, next) {
     const {usuario, password} = req.body;
@@ -13,27 +14,34 @@ router.get('/validate/admin', function(req, res, next) {
     res.send({ "Verify": response });
 });
 
-router.post('/new', function(req, res, next) {
+router.post('/new', async function(req, res, next) {
     const {nombre, apellido, fecha_nacimiento, correo_electronico, sexo, tarjeta_credito, password, id_tipo_usuario} = req.body;
     let query;
-    if(tarjeta_credito != null){
-        query = `INSERT INTO usuario (nombre, apellido, fecha_nacimiento, correo_electronico, sexo, tarjeta_credito, password, tipo_usuario_id_tipo)
-                 VALUES (${nombre}, ${apellido}, ${fecha_nacimiento}, ${correo_electronico}, ${sexo}, ${tarjeta_credito}, ${password}, ${id_tipo_usuario})`;
-    } else {
-        query = `INSERT INTO usuario (nombre, apellido, fecha_nacimiento, correo_electronico, sexo, password, tipo_usuario_id_tipo)
-                 VALUES (${nombre}, ${apellido}, ${fecha_nacimiento}, ${correo_electronico}, ${sexo}, ${password}, ${id_tipo_usuario})`;
-    }
-    console.log(query)
-    con.query(query, function (err, result, fields) {
-        if (err) throw err;
 
-        console.log('aca todo bien')
-
-        con.query("SELECT id_usuario FROM usuario ORDERY BY id_usuario DESC LIMIT 1", function (err, result, fields) {
-            if (err) throw err;
-            res.send( result);
-        })
-    })
+    //1. Validar que el usuario no se encuentre registrado
+    query = `SELECT * FROM usuario WHERE correo_electronico = '${correo_electronico}'`;
+    const response = con.query(query, function(err, result, fields) {
+        if (result.length > 0) {
+            res.send( {"error": "El usuario ya existe"});
+        } else {
+            //2. Registrar usuario
+            if(tarjeta_credito != null){
+                query = `INSERT INTO usuario (nombre, apellido, fecha_nacimiento, correo_electronico, sexo, tarjeta_credito, password, tipo_usuario_id_tipo)
+                        VALUES ('${nombre}', '${apellido}', '${fecha_nacimiento}', '${correo_electronico}', '${sexo}', '${tarjeta_credito}', '${password}', '${id_tipo_usuario}')`;
+            } else {
+                query = `INSERT INTO usuario (nombre, apellido, fecha_nacimiento, correo_electronico, sexo, password, tipo_usuario_id_tipo)
+                        VALUES ('${nombre}', '${apellido}', '${fecha_nacimiento}', '${correo_electronico}', '${sexo}', '${password}', ${id_tipo_usuario})`;
+            }
+            con.query(query, function (err, result, fields) {
+                if (err) throw err;
+                con.query("SELECT id_usuario FROM usuario ORDER BY id_usuario DESC LIMIT 1", function (err, result, fields) {
+                    if (err) throw err;
+                    res.send( result);
+                })
+            })
+        }
+    });
+    
 });
 
 
