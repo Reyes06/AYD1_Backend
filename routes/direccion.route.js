b835aeff480903f69fcc1f435c7acd5c506ecedc
Reyes06 <eddyarnoldo06@gmail.com>
2020-12-15 22:20:16 -0600

    Departamentos y municipios

--- routes/direccion.route.js
+++ routes/direccion.route.js
@@ -10,7 +10,14 @@ router.get('/pais', function(req, res, next) {
 });
 
 router.get('/departamentos/:id_pais', function(req, res, next) {
-  con.query(`SELECT * FROM departamento WHERE pais_id_pais = ${req.params.id_pais}`, function (err, result, fields) {
+  con.query(`SELECT id_depto, nombre FROM departamento WHERE pais_id_pais = ${req.params.id_pais}`, function (err, result, fields) {
+    if (err) throw err;
+    res.send( result);
+  })
+});
+
+router.get('/municipios/:id_depto', function(req, res, next) {
+  con.query(`SELECT id_municipio, nombre FROM municipio WHERE departamento_id_depto = ${req.params.id_depto}`, function (err, result, fields) {
     if (err) throw err;
     res.send( result);
   })
