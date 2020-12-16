df303d6410cf2e8dcfff1eef1c1bf1c0598cd63e
ZantetsukenGT <superspeed52@gmail.com>
2020-12-15 22:13:06 -0600

    Correccion de bugs

--- routes/direccion.route.js
+++ routes/direccion.route.js
@@ -3,12 +3,17 @@ var router = express.Router();
 var con = require('../dbcontroller/dbconnection');
 
 router.get('/pais', function(req, res, next) {
-    console.log('llego aca');
     con.query("SELECT * FROM pais", function (err, result, fields) {
       if (err) throw err;
       res.send( result);
     })
-    
+});
+
+router.get('/departamentos/:id_pais', function(req, res, next) {
+  con.query(`SELECT * FROM departamento WHERE pais_id_pais = ${req.params.id_pais}`, function (err, result, fields) {
+    if (err) throw err;
+    res.send( result);
+  })
 });
   
 module.exports = router;
