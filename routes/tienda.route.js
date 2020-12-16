28aaa6a61021fce60999a0c9d20fa649910a55bb
Reyes06 <eddyarnoldo06@gmail.com>
2020-12-16 00:02:54 -0600

    Nuevas tiendas

--- routes/tienda.route.js
+++ routes/tienda.route.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var con = require('../dbcontroller/dbconnection');
 
 router.post('/nuevo', function(req, res, next) {
     const {nombre, logo, direccion, id_usuario, id_sector, id_municipio } = req.body;
@@ -7,15 +8,18 @@ router.post('/nuevo', function(req, res, next) {
     let query;
     if(logo != null){
         query = `INSERT INTO tienda (nombre, logo, direccion, usuario_id_usuario, municipio_id_municipio, sector_id_sector)
-                       VALUES (${nombre}, ${logo}, ${direccion}, ${id_usuario}, ${id_sector}, ${id_municipio})`;
+                       VALUES ('${nombre}', ${logo}, '${direccion}', ${id_usuario}, ${id_sector}, ${id_municipio})`;
     } else {
         query = `INSERT INTO tienda (nombre, direccion, usuario_id_usuario, municipio_id_municipio, sector_id_sector)
-                       VALUES (${nombre}, ${direccion}, ${id_usuario}, ${id_sector}, ${id_municipio})`;
+                       VALUES ('${nombre}', '${direccion}', ${id_usuario}, ${id_sector}, ${id_municipio})`;
     }
     
     con.query(query, function (err, result, fields) {
         if (err) throw err;
-        res.send( result);
+        con.query("SELECT id_tienda FROM tienda ORDER BY id_tienda DESC LIMIT 1", function (err, result, fields) {
+            if (err) throw err;
+            res.send( result);
+        })
     })
 })
 
