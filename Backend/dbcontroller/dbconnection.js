3dbbbd874ac3e7d508afaad3d2b8b84c43b0d15f
Reyes06 <eddyarnoldo06@gmail.com>
2020-12-22 11:53:11 -0600

    Asyc-Await modulo direccion

--- Backend/dbcontroller/dbconnection.js
+++ Backend/dbcontroller/dbconnection.js
@@ -1,10 +1,10 @@
 var mysql = require('mysql');
 
-var con = mysql.createConnection({
+objectConnection = {
   host: "bvwds6jhztumdpjbhhvu-mysql.services.clever-cloud.com",
   user: "unzioikaweymhb8b",
   password: "3mjXFFedT4pJh7XtdE9P",
   database: "bvwds6jhztumdpjbhhvu"
-});
+};
 
-module.exports = con;
+module.exports = objectConnection;
--- Backend/routes/direccion.route.js
+++ Backend/routes/direccion.route.js
@@ -1,15 +1,18 @@
 var express = require('express');
 var router = express.Router();
-var con = require('../dbcontroller/dbconnection');
+var objectConnection = require('../dbcontroller/dbconnection');
+var mysql = require('mysql');
 
-router.get('/pais', function(req, res, next) {
-  con.connect(function(err) {
-    if (err) throw err;
+router.get('/pais', async function(req, res, next) {
+  con = await mysql.createConnection(objectConnection);
+
+  await con.connect(function(err) {
+    if (err) {console.log(err); throw err};
     console.log("DB Connection OK")
   });
   const query = "SELECT * FROM pais";
   console.log(query);
-  con.query(query, function (err, result, fields) {
+  await con.query(query, function (err, result, fields) {
     if (err) throw err;
     res.send( result);
   })
@@ -19,14 +22,16 @@ router.get('/pais', function(req, res, next) {
   });
 });
 
-router.get('/departamentos/:id_pais', function(req, res, next) {
-  con.connect(function(err) {
+router.get('/departamentos/:id_pais', async function(req, res, next) {
+  con = await mysql.createConnection(objectConnection);
+
+  await con.connect(function(err) {
     if (err) throw err;
     console.log("DB Connection OK")
   });
   const query = `SELECT id_depto, nombre FROM departamento WHERE pais_id_pais = ${req.params.id_pais}`;
   console.log(query);
-  con.query(query, function (err, result, fields) {
+  await con.query(query, function (err, result, fields) {
     if (err) throw err;
     res.send( result);
   })
@@ -36,14 +41,16 @@ router.get('/departamentos/:id_pais', function(req, res, next) {
   });
 });
 
-router.get('/municipios/:id_depto', function(req, res, next) {
-  con.connect(function(err) {
+router.get('/municipios/:id_depto', async function(req, res, next) {
+  con = await mysql.createConnection(objectConnection);
+
+  await con.connect(function(err) {
     if (err) throw err;
     console.log("DB Connection OK")
   });
   const query = `SELECT id_municipio, nombre FROM municipio WHERE departamento_id_depto = ${req.params.id_depto}`;
   console.log(query);
-  con.query(query, function (err, result, fields) {
+  await con.query(query, function (err, result, fields) {
     if (err) throw err;
     res.send( result);
   })
@@ -53,14 +60,16 @@ router.get('/municipios/:id_depto', function(req, res, next) {
   });
 });
 
-router.get('/municipio/', function(req, res, next) {
-  con.connect(function(err) {
+router.get('/municipio/', async function(req, res, next) {
+  con = await mysql.createConnection(objectConnection);
+
+  await con.connect(function(err) {
     if (err) throw err;
     console.log("DB Connection OK")
   });
   const query = "SELECT * FROM municipio";
   console.log(query);
-  con.query(query, function (err, result, fields) {
+  await con.query(query, function (err, result, fields) {
     if (err) throw err;
     res.send( result);
   })
