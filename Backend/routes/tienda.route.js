48982e102dcccdc2045ea87dee9db1fd8d1373c2
Reyes06 <eddyarnoldo06@gmail.com>
2020-12-26 22:31:20 -0600

    'Tienda'

--- Backend/routes/tienda.route.js
+++ Backend/routes/tienda.route.js
@@ -9,7 +9,7 @@ router.get('/:id_usuario', function(req, res, next) {
     con = mysql.createConnection(objectConnection);
     con.connect();
 
-    con.query(`SELECT id_tienda, nombre_tienda, logo, direccion, municipio.nombre, departamento.nombre, pais.nombre, sector.nombre, fecha_aprobacion FROM tienda INNER JOIN formulario ON formulario.id_formulario = tienda.id_tienda INNER JOIN sector ON sector.id_sector = formulario.sector_id_sector INNER JOIN municipio ON municipio.id_municipio = formulario.municipio_id_municipio INNER JOIN departamento ON departamento.id_depto = municipio.departamento_id_depto INNER JOIN pais ON pais.id_pais = departamento.pais_id_pais`, function (err, result, fields) {
+    con.query(`SELECT id_tienda, nombre_tienda, logo, direccion, municipio.nombre, departamento.nombre, pais.nombre, sector.nombre, fecha_aprobacion FROM tienda INNER JOIN formulario ON formulario.id_formulario = tienda.id_tienda INNER JOIN sector ON sector.id_sector = formulario.sector_id_sector INNER JOIN municipio ON municipio.id_municipio = formulario.municipio_id_municipio INNER JOIN departamento ON departamento.id_depto = municipio.departamento_id_depto INNER JOIN pais ON pais.id_pais = departamento.pais_id_pais WHERE usuario_id_usuario = ${id_usuario}`, function (err, result, fields) {
         console.log("SELECT FROM tienda, formulario, sector, municipio, departamento, pais")
         if (err) throw err;
         res.send( {"tiendas": result});
