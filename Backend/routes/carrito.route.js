af4f6611f7f9a3531582a26d5b19f3ac809840fa
Reyes06 <eddyarnoldo06@gmail.com>
2020-12-29 21:32:50 -0600

    Modulo carrito de compras

--- Backend/routes/carrito.route.js
+++ Backend/routes/carrito.route.js
@@ -9,7 +9,7 @@ router.get('/:id_usuario', function(req, res, next) {
     con = mysql.createConnection(objectConnection);
 
     con.connect();
-    con.query(`SELECT pr.id_producto AS id_producto, pr.nombre AS nombre_producto, pr.descripcion AS descripcion, pr.imagen AS imagen, pr.precio AS precio, cc.cantidad, f.nombre_tienda AS tienda, dept.nombre AS nombre_depto FROM carrito_compras cc INNER JOIN producto pr ON pr.id_producto = cc.producto_id_producto INNER JOIN depto_tienda dept ON dept.id_depto = pr.depto_tienda_id_depto INNER JOIN tienda ti ON ti.id_tienda = dept.tienda_id_tienda INNER JOIN formulario f ON f.id_formulario = ti.id_tienda WHERE ti.usuario_id_usuario = ${id_usuario}`, function (err, result, fields) {
+    con.query(`SELECT pr.id_producto AS id_producto, pr.nombre AS nombre_producto, pr.descripcion AS descripcion, pr.imagen AS imagen, pr.precio AS precio, cc.cantidad, f.nombre_tienda AS tienda, dept.nombre AS nombre_depto FROM carrito_compras cc INNER JOIN producto pr ON pr.id_producto = cc.producto_id_producto INNER JOIN depto_tienda dept ON dept.id_depto = pr.depto_tienda_id_depto INNER JOIN tienda ti ON ti.id_tienda = dept.tienda_id_tienda INNER JOIN formulario f ON f.id_formulario = ti.id_tienda WHERE cc.usuario_id_usuario = ${id_usuario}`, function (err, result, fields) {
         console.log("SELECT FROM carrito_compras, producto, departamento, tienda, formulario")
         if (err) throw err;
 
@@ -24,12 +24,25 @@ router.post('/agregar', function(req, res, next) {
     con = mysql.createConnection(objectConnection);
 
     con.connect();
-    con.query(`INSERT INTO carrito_compras (usuario_id_usuario, producto_id_producto, cantidad) VALUES (${id_usuario}, ${id_producto}, ${cantidad}) `, function (err, result, fields) {
-        console.log("INSERT INTO carrito_compras")
+
+    con.query(`SELECT cantidad_inventario FROM producto WHERE id_producto = ${id_producto}`, (err, result, fields) => {
+        console.log("SELECT FROM producto")
         if (err) throw err;
 
-        res.send( {"estado": "ok"});
-        con.end();
+        if(result[0].cantidad_inventario >= cantidad){
+            con.query(`INSERT INTO carrito_compras (usuario_id_usuario, producto_id_producto, cantidad) VALUES (${id_usuario}, ${id_producto}, ${cantidad}) `, function (err, result, fields) {
+                console.log("INSERT INTO carrito_compras")
+                if (err) throw err;
+                res.send( {"estado": "ok"});
+                con.end();
+            });
+        } else {
+            res.send( {
+                "estado": "error",
+                "descripcion": "no hay suficientes productos en inventario"
+            });
+            con.end();
+        }
     });
 });
 
@@ -49,14 +62,29 @@ router.post('/borrar', function(req, res, next) {
     });
 });
 
+/*DELETE: Eliminar todos los productos asociados al carrito de un usuario*/
+router.post('/borrar/all', function(req, res, next) {
+    const {id_usuario} = req.body;
+    con = mysql.createConnection(objectConnection);
+
+    con.connect();
+    con.query(`DELETE FROM carrito_compras WHERE usuario_id_usuario = ${id_usuario}`, function (err, result, fields) {
+        console.log("DEETE FROM carrito_compras")
+        if (err) throw err;
+
+        res.send( {"estado": "ok"});
+        con.end();
+    });
+});
+
 /*UPDATE: Cambiar la cantidad de unidades un producto asociado al carrito de un usuario*/
-router.post('/borrar', function(req, res, next) {
+router.post('/update', function(req, res, next) {
     const {id_usuario, id_producto, nueva_cantidad} = req.body;
     con = mysql.createConnection(objectConnection);
 
     con.connect();
     con.query(`UPDATE carrito_compras SET cantidad = ${nueva_cantidad} WHERE usuario_id_usuario = ${id_usuario} AND producto_id_producto = ${id_producto}`, function (err, result, fields) {
-        console.log("DEETE FROM carrito_compras")
+        console.log("UPDATE FROM carrito_compras")
         if (err) throw err;
 
         res.send( {"estado": "ok"});
